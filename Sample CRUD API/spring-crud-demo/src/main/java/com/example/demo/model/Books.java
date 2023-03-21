3183284e25536aa83b4193dd875822eee044f925
Krish <114279261+Krishrx@users.noreply.github.com>
2023-03-22 00:09:32 +0530

    Add files via upload

---
+++ Sample CRUD API/spring-crud-demo/src/main/java/com/example/demo/model/Books.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Books {
+	@Id
+	@GeneratedValue
+	@Column
+	private int bookId;
+	@Column
+	private String bookName;
+	@Column
+	private String author;
+	@Column
+	private int price;
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
